2d117efabbf75bd9537feeb5fb95502113ed4e53
onmedicines <semwalanurag26@gmail.com>
2024-09-23 15:27:13 +0530

    Fix-export schema as 'User'

--- schemas/User.js
+++ schemas/User.js
@@ -5,4 +5,4 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true },
 });
 
-export default mongoose.model(User, userSchema);
+export default mongoose.model("User", userSchema);

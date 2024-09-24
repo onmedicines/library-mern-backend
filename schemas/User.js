5c93324193cd7dea47817a2ba5bc0b85159c75c5
onmedicines <semwalanurag26@gmail.com>
2024-09-24 12:07:14 +0530

    Store the books of the user inside an array in the User document

--- schemas/User.js
+++ schemas/User.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
 
+const bookSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  author: { type: String, required: true },
+  pages: { type: Number, required: true },
+});
+
 const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  books: [bookSchema],
 });
 
 export default mongoose.model("User", userSchema);

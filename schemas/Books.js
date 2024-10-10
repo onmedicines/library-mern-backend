22567d3cb78ee873a0fa5ed5f3e72290c9121da3
onmedicines <semwalanurag26@gmail.com>
2024-10-10 18:19:50 +0530

    Add fields for summary and rating

--- schemas/Books.js
+++ schemas/Books.js
@@ -4,6 +4,8 @@ const bookSchema = mongoose.Schema({
   name: { type: String, required: true },
   author: { type: String, required: true },
   pages: { type: Number, required: true },
+  summary: { type: String, required: false },
+  rating: { type: Number, required: true },
 });
 
 export default mongoose.model("Books", bookSchema);

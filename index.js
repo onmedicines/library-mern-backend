4e4a29c5dcc1bb76a80de99ed0cbe5b6217ff3fb
onmedicines <semwalanurag26@gmail.com>
2024-10-21 13:29:18 +0530

    Add jwt authentication and authorization

--- index.js
+++ index.js
@@ -24,6 +24,25 @@ app.listen(process.env.PORT, () => {
   console.log(`server started on port ${process.env.PORT} 🎉`);
 });
 
+// Functions
+function authenticate(req, res, next) {
+  try {
+    const authHeader = req.headers["authorization"]; // format: BEARER <token>
+    // console.log(authHeader);
+    const token = authHeader && authHeader.split(" ")[1];
+    // console.log(token);
+    if (!token) throw new Error("cannot be authorized!!");
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) return res.status(401).json({ errorAuthenticateToken: err.message });
+      req.username = user.username;
+      next();
+    });
+  } catch (err) {
+    res.status(401).json({ errorAuthenticate: err.message });
+  }
+}
+
+// Routes
 app.route("/").get((req, res) => {
   res.json({ message: "hello" });
 });
@@ -36,13 +55,15 @@ app.route("/signup").post(async (req, res) => {
      * if user already registered
      * throw an error
      */
-    const user = await User.findOne({ username });
+    let user = await User.findOne({ username });
     if (user) throw new Error("User already Exists");
 
-    await User.create({ username, password });
-    res.json({ message: "user created" });
+    user = await User.create({ username, password });
+    jwt.sign({ username }, process.env.SECRET_KEY, (err, token) => {
+      res.status(200).json({ message: "user created", user, token });
+    });
   } catch (err) {
-    res.status(409).json({ error: err.message });
+    res.status(409).json({ errorSignup: err.message });
   }
 });
 
@@ -56,30 +77,15 @@ app.route("/signin").post(async (req, res) => {
 
     // username and password are valid beyound this point
     // we can generate a jwt
-    const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET);
-
-    res.json({ message: "user logged in!!", books: user.books, accessToken });
+    jwt.sign({ username }, process.env.SECRET_KEY, (err, token) => {
+      res.status(200).json({ message: "user logged in!!", user, token });
+    });
   } catch (err) {
-    res.json({ error: err.message });
+    res.json({ errorSignin: err.message });
   }
 });
 
-function authorizeToken(req, res, next) {
-  try {
-    const authHeader = req.headers["authorization"]; // format: BEARER <token>
-    const token = authHeader && authHeader.split(" ")[1];
-    if (!token) throw new Error("cannot be authorized!!");
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) throw err;
-      req.username = user.username;
-      next();
-    });
-  } catch (err) {
-    res.json({ error: err.message });
-  }
-}
-
-app.route("/add-book").post(async (req, res) => {
+app.route("/user").post(authenticate, async (req, res) => {
   try {
     const name = req.body.bookName;
     const author = req.body.author;
@@ -87,15 +93,15 @@ app.route("/add-book").post(async (req, res) => {
 
     if (!name || !author || !pages) throw new Error("one or more fields missing");
 
-    const dbResponse = await Books.create({ name: name, author: author, pages: pages });
+    const dbResponse = await User.findOneAndUpdate({ username: req.username }, { $push: { books: { name: name, author: author, pages: pages } } }, { new: true });
     res.json({ message: "book added to db" });
   } catch (err) {
     console.error(err.message);
-    res.json({ message: err.message });
+    res.json({ errorUser: err.message });
   }
 });
 
-app.route("/books").post(authorizeToken, async (req, res) => {
+app.route("/books").post(authenticate, async (req, res) => {
   const username = req.username;
   const user = await User.findOne({ username });
 
